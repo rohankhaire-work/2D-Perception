fixed disp normalization

disparity_refinement/dataloader/data_loader.py
@@ -30,17 +30,19 @@ def input_sgm(data_path):
     # Load disp map
     im = cv2.imread(data_path, cv2.IMREAD_ANYDEPTH)
     h, w = im.shape
-    max_disp = 192
+
+    dataset_mean = 30.5 * (512. / w)
+    dataset_std = 7.5 * (512. / w)
     # Convert to float
     disp_float = im.astype(np.float32) / 256.0
 
     # Resize
     size = (512, 256)
-    disp_resized = cv2.resize(disp_float, size,
-                              interpolation=cv2.INTER_NEAREST)
+    disp_resized = 512/w * cv2.resize(disp_float, size,
+                                      interpolation=cv2.INTER_LINEAR)
 
     # Normalize disparity
-    disp_norm = np.clip(disp_resized, 0, max_disp) / max_disp
+    disp_norm = (disp_resized - dataset_mean) / dataset_std
     disp_norm = disp_norm.reshape(disp_norm.shape+(1,))
 
     return disp_norm
@@ -50,31 +52,36 @@ def train_target_gt_depth_loader(data_path):
 
     # Load disp map
     im = cv2.imread(data_path, cv2.IMREAD_ANYDEPTH)
-
+    h, w = im.shape
+    dataset_std = 7.5 * (512. / w)
+    dataset_mean = 30.5 * (512. / w)
     # Convert to float
     disp_float = im.astype(np.float32) / 256.0
 
     # Resize
     size = (512, 256)
-    disp_resized = cv2.resize(
-        disp_float, size, interpolation=cv2.INTER_NEAREST)
+    disp_resized = 512/w * cv2.resize(disp_float, size,
+                                      interpolation=cv2.INTER_NEAREST)
+    disp_resized[disp_resized == 0.] = -10000.
+    # Normalize disparity
+    disp_norm = (disp_resized - dataset_mean) / dataset_std
 
-    disp_resized = disp_resized.reshape(disp_resized.shape+(1,))
+    disp_norm = disp_norm.reshape(disp_norm.shape+(1,))
 
-    return disp_resized
+    return disp_norm
 
 
 def test_target_gt_depth_loader(data_path):
 
     # Load disp map
     im = cv2.imread(data_path, cv2.IMREAD_ANYDEPTH)
-
+    h, w = im.shape
     # Convert to float
     disp_float = im.astype(np.float32) / 256.0
 
     # Resize
     size = (512, 256)
-    disp_resized = cv2.resize(
+    disp_resized = (512/w) * cv2.resize(
         disp_float, size, interpolation=cv2.INTER_NEAREST)
 
     disp_resized = disp_resized.reshape(disp_resized.shape+(1,))

disparity_refinement/executors/train.py
@@ -30,6 +30,10 @@ class EarlyStopping:
 
 def train(model, optimizer, epoch, train_loader):
 
+    dataset_mean = 30.5 * (512. / 1242.)
+    dataset_std = 7.5 * (512. / 1242.)
+
+    zero_disp = -1. * (dataset_mean / dataset_std)
     total_loss_disp = 0
 
     img_counter = 0
@@ -41,8 +45,7 @@ def train(model, optimizer, epoch, train_loader):
                         total=len(train_loader))
 
     for batch_idx, (input_data, target_data) in progress_bar:
-        concat_data = torch.cat((input_data[:]), 1)
-        input_data = concat_data.cuda()
+        input_data = [inp_data.cuda() for inp_data in input_data]
         target_data = [ta_data.cuda() for ta_data in target_data]
 
         # Disp Map Prediction
@@ -54,7 +57,9 @@ def train(model, optimizer, epoch, train_loader):
         target = target_data[0]
 
         # We only compute error on non-zero pixels (i.e. non 1 here)
-        error_disp = combined_berhu_smoothl1_loss(output_disp, target)
+        mask = target > zero_disp
+        error_disp = combined_berhu_smoothl1_loss(
+            output_disp[mask], target[mask])
 
         # loss
         total_loss_disp += error_disp.item()
@@ -78,6 +83,8 @@ def test(model, epoch, test_loader):
     dt = 0.
     d1_cnn = 0.
 
+    dataset_std = 7.5 * (512. / 1242.)
+    dataset_mean = 30.5 * (512. / 1242.)
     min_disp = 0.0
     max_disp = 192.0
 
@@ -86,8 +93,7 @@ def test(model, epoch, test_loader):
 
     with torch.no_grad():
         for batch_idx, (input_data, target_data) in progress_bar:
-            concat_data = torch.cat((input_data[:]), 1)
-            input_data = concat_data.cuda()
+            input_data = [inp_data.cuda() for inp_data in input_data]
             target_data = [ta_data.cuda() for ta_data in target_data]
 
             # Disp Map Prediction
@@ -119,7 +125,8 @@ def test(model, epoch, test_loader):
                 # Get the Pred depth
                 #########################
                 pred_disp = np.squeeze(out_disp[i], axis=0)
-
+                pred_disp = pred_disp * dataset_std
+                pred_disp = pred_disp + dataset_mean
                 # Bound the predicted depth
                 pred_disp[pred_disp < min_disp] = min_disp
                 pred_disp[pred_disp > max_disp] = max_disp
@@ -164,10 +171,6 @@ def train_network(model, optimizer, scheduler, epochs, train_loader, test_loader
 
 
 def combined_berhu_smoothl1_loss(pred, gt, alpha=0.5):
-    # Valid disparity mask
-    mask = gt > 0
-    pred = pred[mask]
-    gt = gt[mask]
 
     if pred.numel() == 0:
         return torch.tensor(0.0, device=pred.device, requires_grad=True)

disparity_refinement/main.py
@@ -5,6 +5,7 @@ from dataloader.transforms import ToTensor
 from dataloader.co_transforms import Compose, RandomColorJitter, \
     RandomHorizontalFlip
 from model.DilNet import DilNetLRDisp
+from model.StereoRefineNet import StereoRefineNet
 from executors.train import train_network, EarlyStopping
 
 import torch
@@ -79,10 +80,12 @@ test_loader = torch.utils.data.DataLoader(
 
 # Create model
 net_refine = DilNetLRDisp(7, 1).to(device)
+# net_refine = StereoRefineNet().cuda()
 
-optimizer = torch.optim.Adam(net_refine.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.MultiStepLR(
-    optimizer, milestones=milestones, gamma=0.5)
+optimizer = torch.optim.Adam(
+    net_refine.parameters(), lr=lr, betas=(0.9, 0.999))
+scheduler = torch.optim.lr_scheduler.StepLR(
+    optimizer, step_size=80, gamma=0.5)
 
 # Early stopping
 early_stopping = EarlyStopping(patience=patience, min_delta=0.01,